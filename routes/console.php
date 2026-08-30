b1e0969c8889dde63646c8ad181fcd5deed9766e
Temidayo Victor <temidayovictor1@gmail.com>
2026-08-30 13:39:19 +0100

    Fix: Correct Schedule Time for Subscription Expiry Job

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new SubscriptionExpiryJob)->everyMinute();
+Schedule::job(new SubscriptionExpiryJob)->dailyAt('08:00');
